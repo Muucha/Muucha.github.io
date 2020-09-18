4b0965d00e7708c11d1bf4e832dcbe771c43d0d8
Muucha <54197234+Muucha@users.noreply.github.com>
2020-09-18 22:18:31 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,19 @@
 var respecConfig = {
   specStatus: "NOTE",
   editors: [{
-    name:       "Riccardo Albertoni",
-    orcid:  "0000-0001-5648-2713",
-    company:    "CNR - Consiglio Nazionale delle Ricerche, Italy",
-    url: "http://www.imati.cnr.it/joomla/index.php/people/8-curricula/178-riccardo-albertoni",
-    companyURL: "http://www.cnr.it/",
-    w3cid: 72458
+    name:       "Hoge Hoge",
+    orcid:  "0000-0000-0000-0000",
+    company:    "XXX - XXX, Japan",
+    url: "http://www.xxxxx.co.jp/",
+    companyURL: "http://www.xxxx.co.jp/",
+    w3cid: 00000
   }],
   otherLinks: [{
     key: "Contributors",
     data: [
     {
-      value: "Lars G. Svensson",
-      href:  "https://orcid.org/0000-0002-8714-9718"
-    },
-    {
-      value: "Nicholas J. Car",
-      href:  "https://orcid.org/0000-0002-8742-7730"
+      value: "AAA A. AAAAAAA",
+      href:  "https://aaa.org/0000-000"
     }
     ]
   }],
@@ -26,38 +22,12 @@ var respecConfig = {
   shortName: "dcat-implementation-report",
   localBiblio: {
     "CiteDCAT-AP": {
-      "href": "https://ec-jrc.github.io/datacite-to-dcat-ap/",
-      "title": "DataCite to DCAT-AP Mapping",
-      "authors": ["Andrea Perego","Timothy Austin"],
+      "href": "https://muucha.github.io/",
+      "title": "XXXXXXXXXXXX",
+      "authors": ["BBB BBBB","CCCC CCCCC"],
       "status": "Working Draft",
-      "publisher":"European Commission, Joint Research Centre (JRC)",
+      "publisher":"XXXXXXXXXXXXXXXXXXXXXx",
       "date": "2019"
-    },
-    "DCAT-AP-EXT":{
-      "href":"https://joinup.ec.europa.eu/solution/dcat-application-profile-data-portals-europe/document/national-extensions-analysis-dcat-ap",
-      "title":"Analysis of the DCAT-AP extensions",
-      "publisher":"European Commission",
-      "date":"2017"
-    },
-    "DCAT-AP-IG":{
-      "href":"https://joinup.ec.europa.eu/solution/dcat-application-profile-implementation-guidelines",
-      "title":"DCAT application profile implementation guidelines",
-      "publisher":"European Commission",
-      "date":"2016"
-    },
-    "DCAT-AP-JRC": {
-      "href": "https://ec-jrc.github.io/dcat-ap-jrc/",
-      "title": "DCAT-AP-JRC: An extension to DCAT-AP for multidisciplinary research data",
-      "authors": ["Andrea Perego"],
-      "status": "Working Draft",
-      "publisher":"European Commission, Joint Research Centre (JRC)",
-      "date": "2019"
-    },
-    "DCAT-AP-USE":{
-      "href":"https://joinup.ec.europa.eu/document/report-dcat-ap-use",
-      "title":"Report on DCAT-AP Use",
-      "publisher":"European Commission",
-      "date":"2017"
     }
   }
 };
